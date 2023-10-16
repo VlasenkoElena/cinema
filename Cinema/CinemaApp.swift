762d49a4590825daa97ed5d1118472b677bbd66b
VlasenkoHelena <33989161+VlasenkoElena@users.noreply.github.com>
2023-10-16 15:22:46 +0200

    chore: add new project

---
+++ Cinema/CinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaApp.swift
+//  Cinema
+//
+//  Created by Helen on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
